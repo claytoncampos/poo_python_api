dd3d749851c51742fd83fd0dabf8aadf60068e51
Clayton De Almeida Campos <clayton.campos@telefonica.com>
2024-05-06 17:03:42 -0300

    add class prato, item_cardapio, bebida

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
-restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+
+restaurante_praca = Restaurante("praça", "Gourmet")
+bebida_suco = Bebida("sudo de maça", 6.00, "grande")
+prato_paozinho = Prato("Paozinho", 2.00, "Melhor pão da cidade")
+
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
